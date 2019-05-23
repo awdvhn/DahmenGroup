e14c9e6f28d9685d03a329d30b42d1d443f8b6c5
mayishazn <43865608+mayishazn@users.noreply.github.com>
2019-05-23 14:20:46 -0500

    Update and rename clean_bmgdata.py to clean_data.py

---
+++ clean_data.py
@@ -0,0 +1,24 @@
+#Alan Long 6/15/16
+#Last edited: Alan Long 5/16/18
+
+#This code takes in an array and removes all non-positive, infinite, and not a
+#numbers. It accepts an array and outputs an array of the same length.
+
+
+
+
+
+import numpy as np
+
+def clean_data(data):
+    if np.isnan(data).sum()>0:
+        data=data[~np.isnan(data)]
+    if np.isinf(data).sum()>0:
+        data=data[~np.isinf(data)]
+    if (data<=0).sum()>0:
+        data=data[data>0]        
+
+
+    return data
+
+
